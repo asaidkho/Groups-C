38e876de7bac7b82859b9c972766b6268d2d188f
Gunel <gunes.ahmedova@gmail.com>
2020-08-04 02:20:31 -0400

    changes to FilterPage

---
+++ src/test/java/pages/FilterPage.java
@@ -0,0 +1,73 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class FilterPage extends BasePage { 
+
+	
+	//drawer label
+	@FindBy(id = "mpghighway")
+	public WebElement MPGHighway;
+	
+	
+	//slider
+	@FindBy(xpath = "//*[@id=\"mpghighway\"]/div[3]/div/div/div[2]/div[4]")
+	public WebElement sliderHandle;
+	
+	//inputBox
+	//@FindBy(className = "slider-input__text")
+	@FindBy(xpath = "//input[@type='tel']")
+	public WebElement inputBox;
+	
+	
+	//Features
+	@FindBy(id = "Features")
+	public WebElement features;
+	
+	//seeAllFromFeatures
+	
+	@FindBy(xpath="//*[@id=\"Features\"]/div[3]/ul/button")
+	//@FindBy(className ="facet--curated-show-more kmx-button kmx-button--secondary visible");
+	//@FindBy(xpath="//button[.='See All']");
+	public WebElement seeAll;
+	
+	
+	
+	//clear filter
+	@FindBy(className="sidebar-header--action clear--link")
+	public WebElement clear;
+	
+	
+	//drop down 
+		@FindBy(className="kmx-menu-button kmx-button kmx-button--tertiary")
+		public WebElement DropDown;
+		
+		
+		//nationwide
+		@FindBy(xpath="//*[@id=\"search-bar-form\"]/div/div[2]/div[2]/div/div/div/ul/li[9]/button")
+		public WebElement nationwide;
+	
+		/*@FindBy (css = "#search-bar-form 
+		 * > div > div.search-bar--fields-wrapper > div.search-bar--form-field.radius > div > button")
+		public WebElement DropDown;
+		
+		
+		@FindBy (xpath = "//*[@id=\"search-bar-form\"]/div/div[2]/div[2]/div/div/div/ul/li[9]/button")
+		public WebElement distanceRadiusNationwide;*/
+	}
+
+	
+	
+	
+	//change place to searchPage.
+	//search byMake>Nationwide
+	//@FindBy(xpath = "")
+	//public WebElement ;
+	
+	
+	
+	
+	
+	
+}
--- src/test/java/tests/MPGHighwayTest.java
+++ src/test/java/tests/MPGHighwayTest.java
@@ -6,75 +6,95 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
+import pages.MainPage;
 import pages.ShopPage;
+import utilities.BrowserUtilities;
 
 public class MPGHighwayTest {
 
 	/* @BeforeMethod
 	 * 
-	 @Test(groups="smoke")
+	 @Test()
 	public void filtersTest() {
 		logger = reporter.createTest("Locate MPG Highway from filters");
 		ShopPage tp = new TrucksPage();
 		tp.MPGHighway.click();
 		logger.info("Clicking MPG HIghway filter Button");
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,3200)");
+		
 		assertTrue(tp.localCars.isDisplayed());
 		assertTrue(tp.localCars.isEnabled());
 		driver.manage().deleteAllCookies();
 		tp.MPGHighway.click();
+		
 		if (shp.locationConformPopUp.isEnabled()) {
 			shp.locationConformPopUp.click();
 		}
-		logger.info("Verifying the expected text");
-		assertTrue(driver.findElement(By.id("search-results-header")).getText().contains("Used cars at CarMax Gaithersburg for Sale"));
+		logger.info("");
+		
 	}
 }
       @Test
       public void MPGHighwayTest(){
-      
-      
-      
-      
-      }
+       }
       
       
       @Test
       public void addToFilter(){
-      
-      
-      
-      
+     
       }
       
+      
+      
+      
+       
       
       @Test 
       public void searchMakeNationwide (){
       
-      
-      
-      }
-      
-      
+      @Test (dataProvider = "file") // add new data to file
+	  public void searchMakeNationwide(String make, String model) {
+		logger = reporter.createTest("Search by Make");
+		MainPage mp = new MainPage();
+		
+		logger.info("Enter car make"); //BMW X5
+		mp.searchBox.sendKeys(make);
+		logger.info("Clicking on search button");
+		mp.searchButton.click();
+		
+		//assertTrue(mp.searchResults.getText().contains(make));
+		assertTrue(driver.getTitle().contains(make));
+		
+		logger.info("Expand distance wrapper icon");
+		ResultPage rp = new ResultPage()
+		rp.nationwide.click();
+		
+		BrowserUtilities.waitFor(2);
+		driver.manage().deleteAllCookies();
+		assertTrue(rp.nationwide.isEnabled());
+		
+		logger.info("Select Nationwide button");
+		mp.blueColor.click();
+		BrowserUtilities.waitFor(2);
+		logger.info("Validate that Nation wide filter is applied");
+		-- dont know how, changes on class name> (kmx-menu-item-button selected)
+		assertTrue(driver.getTitle().contains("nationwide"));
+	
+	}
+        logger.info("Clicking on distance radius dropdown");
+		rp.Dropdown.click();
+		logger.info("Select the distance radius - Nationwide");
+		rp.Nationwide.click();
+		assertEquals(mp.distanceRadiusDropdown.getText(), "Nationwide");
+	
       
       @Test 
       public void clearFilters(){
-      
-      
-      
-      
-      
+       
       
       
       }
-*
-*
-*
-*
-*
-*
-*
+
 */
 	
 	
